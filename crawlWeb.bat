c1ab85be209dc73c11b9e677984cd29893aa7de8
ninhld <luongduyninh@gmail.com>
2023-11-14 00:25:42 +0700

    update crawl data, export to excel

--- crawlWeb.bat
+++ crawlWeb.bat
@@ -5,8 +5,8 @@ SETLOCAL
 set apps_dir=%~dp0
 set url="%1"
 
-::echo python "%apps_dir%crawweb.py" %url%
-python "%apps_dir%crawweb.py" %url%
+::echo python "%apps_dir%crawlWeb.py" %url%
+python "%apps_dir%crawlWeb.py" %url%
 
 ::echo "%apps_dir%.venv\Scripts\activate.bat"
 ::python -V
--- crawlWeb.py
+++ crawlWeb.py
@@ -30,6 +30,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+from openpyxl import Workbook
+from datetime import datetime
 
 SCROLL_SLEEP=5 #second
 
@@ -130,9 +132,9 @@ def load_webpage_by_browser(url):
     scroll_down_counter = 0
     while True:
         # Scroll down to the bottom of the page
-        element_body = driver.find_elements(By.TAG_NAME, "body")
-        for element in element_body:
-            element.send_keys(Keys.END)
+        element_body = driver.find_element(By.TAG_NAME, "body")
+        if not (element_body is None):
+            element_body.send_keys(Keys.END) # go to end of page
 
         # Wait for a short interval to allow the page to load
         time.sleep(SCROLL_SLEEP)
@@ -163,48 +165,144 @@ def save_file(save_content, file_path):
         file.writelines(save_content)
 
 
-def extract_webpage_data_ex1(html_content):
+def extract_webpage_data_ex1(url, html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     tables = soup.find_all("div", class_="pm-table-wrapper")
-    for table_id, table in enumerate(tables):
-        print(f"\n\nTable: {table_id + 1}")
-        rows = table.find_all("tr")
-        for row_id, row in enumerate(rows):
-            col1 = row.find("p")
-            col2 = row.find("td")
-            col2Text = col2.text
-            col2Text = col2Text.replace("\n", " ")
-            print(f"row: {row_id} / {col1.text} - {col2Text}")
 
-def extract_webpage_data_ex2(html_content):
+    product_list = set()
+    product_counter = 0
+
+    for table_id, table in enumerate(tables):
+        # print(f"\n\nTable: {table_id + 1}")
+        rows = table.find_all("tr")
+        for row in rows:
+            col1 = row.find("p").text.strip()
+            col2 = row.find("td").text.strip().replace("\n", " ")
+
+            # print(f"row: {row_id} / {col1} - {col2}")
+            if col1 is None or len(col1) == 0:
+                continue
+            
+            product_counter = product_counter + 1
+            # print(f"Product: {product_counter} - {col1}")
+            product_list.add(col1)
+
+    add_database_by_url(url, product_list)
+    add_database_by_product(url, product_list)
+
+
+def extract_webpage_data_ex2(url, html_content):
     soup = BeautifulSoup(html_content, "html.parser")
     products = soup.find_all(class_="product-card__info")
-    for product_id, product in enumerate(products):
-        print(f"Product: {product_id} - {product.text}")
+    product_list = set()
+    product_counter = 0
+    for product_id, product_item in enumerate(products):
+        product = product_item.text.strip()
+        if product is None or len(product) == 0:
+            continue
+        
+        product_counter = product_counter + 1
+        print(f"Product: {product_counter} - {product}")
+        product_list.add(product)
+    
+    add_database_by_url(url, product_list)
+
+
+database_by_url = dict()     #key: url,     value: set of product
+database_by_product = dict() #key: product, value: set of url
+
+def add_database_by_url(url, product_list):
+    database_by_url[url] = product_list
+
+def add_database_by_product(url, product_list):
+    for product in product_list:
+        if product in database_by_product:
+            url_set = database_by_product[product] #get set
+            url_set.add(url)
+        else:
+            url_set = set()
+            url_set.add(url)
+            database_by_product[product] = url_set #assign set if not exists
+
+
+def fill_database_by_url(sheet):
+    #header
+    sheet["A1"] = "No"
+    sheet["B1"] = "Url"
+    sheet["C1"] = "Product"
+    sheet["D1"] = "Filter"
+
+    #body
+    fill_database(database_by_url, sheet)
+
+def fill_database_by_product(sheet):
+    #header
+    sheet["A1"] = "No"
+    sheet["B1"] = "Product"
+    sheet["C1"] = "Url"
+
+    #body
+    fill_database(database_by_product, sheet)
+
+def fill_database(database_dict, sheet):
+    row_index = 2
+    for key_id, key in enumerate(database_dict):
+        # print(f"{key_id + 1}. {key}")
+        sheet[f"B{row_index}"] = key
+        for set_id, set_item in enumerate(database_dict[key]):
+            # print(f"    {set_id + 1}. {set_item}")
+            no_id = row_index - 1
+            sheet[f"A{row_index}"].value = no_id
+            sheet[f"C{row_index}"].value = set_item
+            row_index = row_index + 1
+
+def save_crawl_to_excel(url_input_file):
+    workbook = Workbook()
+
+    #export crawl data by url
+    sheet = workbook.active
+    sheet.title = "by_url"
+    fill_database_by_url(sheet)
+
+    #export crawl data by product
+    sheet = workbook.create_sheet("by_product")
+    fill_database_by_product(sheet)
+
+    #save excel file
+    workbook.save(filename=make_excel_filename(url_input_file))
+
+
+def make_excel_filename(input_file):
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d_%H%M%S")
+    input_file_no_ext = os.path.splitext(os.path.basename(input_file))[0]
+    return f"{input_file_no_ext}_{dt_string}.xlsx"
 
 def main():
+    print("\n\n----- MAIN Start------")
     arguments  = sys.argv
     if (len(arguments) != 2) or (arguments [1] is None) or (len(arguments [1]) == 0):
         print("Please input url list file")
         return
     
-    print(f"Url list file: {arguments [1]} ")
+    url_input_file = arguments [1]
+    print(f"Url list file: {url_input_file} ")
 
-    urls = get_url_list(arguments [1])
+    urls = get_url_list(url_input_file)
     number_of_url = len(urls)
     print(f"Number of url: {number_of_url} \n\n")
-    for url_id, url in enumerate(urls):
-        url_strip = url.strip()
-        print(f"{url_id + 1}  - {url_strip}")
-        html_content = load_webpage_by_browser(url_strip)
+    for url_id, url_item in enumerate(urls):
+        url = url_item.strip()
+        print(f"{url_id + 1}  - {url}")
+        html_content = load_webpage_by_browser(url)
         if (html_content is None) or (not html_content):
             print(f"Load webpage failed !")
             continue
-    
-        extract_webpage_data_ex1(html_content)
-        print("\n\n")
+        extract_webpage_data_ex1(url, html_content)
+
+    save_crawl_to_excel(url_input_file)
+    print("----- MAIN End------")
 
 if __name__ == '__main__':
-    print("\n\n----- MAIN Start------")
     main()
-    print("----- MAIN End------")
+    
